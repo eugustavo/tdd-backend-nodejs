c4da319817c8c40119eec46d28c26710930010e3
Gustavo Souza <gustavo.souza@crptecnologia.com.br>
2020-06-08 16:08:16 -0300

    ✅ Finished User Test

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  storage: './__tests__/database.sqlite',
+  storage: '../../__tests__/database.sqlite',
   logging: false,
   define: {
     timestamps: true,
